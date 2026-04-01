dba925ffe4d04eac0da27ffa8d59dd4dc58c7227
Waseem Sathar <waseem.sathar>
2026-04-01 18:57:50 +0800

    setup: config for vitest

---
+++ vitest.config.js
@@ -0,0 +1,17 @@
+export default {
+    test: {
+        globals: true,
+        environment: 'node',
+        coverage: {
+            provider: 'v8',
+            all: true,
+            reporter: ['text', 'json', 'html'],
+            include: ['src/**/*.ts'],
+            exclude: [
+                'node_modules/',
+                'dist/',
+                'server.ts'
+            ]
+        }
+    }
+}
