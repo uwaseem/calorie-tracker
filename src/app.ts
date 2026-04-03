ddec0fa77dbe058fc6fecfdf63921a469d7414ee
Ubaidillah Waseem <u.waseem24@gmail.com>
2026-04-03 17:33:39 +0800

    Added a lightweight UI layer which renders a simple button with static client-side response
    * move / to serve UI
    
    * updated tests for '/' to return HTML content
    
    * basic HTML with a button and dummy response
    
    * removing TODO comment which is not relevant any longer
    
    ---------
    
    Co-authored-by: Waseem Sathar <waseem.sathar>

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,17 @@
 import express from "express"
+import path from "path"
+
+import { fileURLToPath } from "url"
 
 import trackRoute from "./routes/track.js"
 
-export const APP = express()
-APP.use(express.json())
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-APP.get(["/", "/health", "/ping"], (_req, res) => {
+export const APP = express()
+APP.use(express.static(path.join(__dirname, "../client")))
+
+APP.get(["/health", "/ping"], (_req, res) => {
   return res
     .status(200).json({
       data: { message: "Welcome to Calorie Tracker!" }
