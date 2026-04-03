02204585e9139837377055aadac8d8ced5b2067f
Waseem Sathar <waseem.sathar>
2026-04-03 17:07:04 +0800

    updated tests for '/' to return HTML content

--- tests/integration/app.test.ts
+++ tests/integration/app.test.ts
@@ -6,69 +6,69 @@ import { APP } from "../../src/app.js"
 
 const message = "Welcome to Calorie Tracker!"
 
+// TODO: add proper test serving the UI for this
 describe("/", () => {
-    it("should return status code 200", async () => {
-        const res = await request(APP).get("/")
+  it("should return status code 200", async () => {
+    const res = await request(APP).get("/")
 
-        expect(res.status).toBe(200)
-    })
+    expect(res.status).toBe(200)
+  })
 
-    it("should return message 'Welcome to Calorie Tracker'", async () => {
-        const res = await request(APP).get("/")
-
-        expect(res.body).toEqual({
-            data: { message }
-        })
-    })
+  it("should return HTML content'", async () => {
+    const res = await request(APP).get("/")
+    const contentType = res.headers["content-type"]
+  
+    expect(contentType).toContain("text/html")
+  })
 })
 
 describe("/health", () => {
-    it("should return status code 200", async () => {
-        const res = await request(APP).get("/health")
+  it("should return status code 200", async () => {
+    const res = await request(APP).get("/health")
 
-        expect(res.status).toBe(200)
+    expect(res.status).toBe(200)
+  })
+
+  it("should return message 'Welcome to Calorie Tracker'", async () => {
+    const res = await request(APP).get("/health")
+
+    expect(res.body).toEqual({
+      data: { message }
     })
-
-    it("should return message 'Welcome to Calorie Tracker'", async () => {
-        const res = await request(APP).get("/health")
-
-        expect(res.body).toEqual({
-            data: { message }
-        })
-    })
+  })
 })
 
 describe("/ping", () => {
-    it("should return status code 200", async () => {
-        const res = await request(APP).get("/ping")
+  it("should return status code 200", async () => {
+    const res = await request(APP).get("/ping")
 
-        expect(res.status).toBe(200)
+    expect(res.status).toBe(200)
+  })
+
+  it("should return message 'Welcome to Calorie Tracker'", async () => {
+    const res = await request(APP).get("/ping")
+
+    expect(res.body).toEqual({
+      data: { message }
     })
-
-    it("should return message 'Welcome to Calorie Tracker'", async () => {
-        const res = await request(APP).get("/ping")
-
-        expect(res.body).toEqual({
-            data: { message }
-        })
-    })
+  })
 })
 
 describe("/unknown-route", () => {
-    it("should return status code 404", async () => {
-        const res = await request(APP).get("/unknown-route")
+  it("should return status code 404", async () => {
+    const res = await request(APP).get("/unknown-route")
 
-        expect(res.status).toBe(404)
+    expect(res.status).toBe(404)
+  })
+
+  it("should return appropiate error message", async () => {
+    const res = await request(APP).get("/unknown-route")
+
+    expect(res.body).toEqual({
+      error: {
+        code: "ROUTE_NOT_FOUND",
+        message: "Route not found"
+      }
     })
-
-    it("should return appropiate error message", async () => {
-        const res = await request(APP).get("/unknown-route")
-
-        expect(res.body).toEqual({
-            error: {
-                code: "ROUTE_NOT_FOUND",
-                message: "Route not found"
-            }
-        })
-    })
+  })
 })
