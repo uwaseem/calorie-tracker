b067d34edd7655278efeef6e260ca6459e101049
Waseem Sathar <waseem.sathar>
2026-04-05 17:41:34 +0800

    added playwright config

---
+++ playwright.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from '@playwright/test'
+
+export default defineConfig({
+  testDir: "./tests/ui",
+  use:{
+    browserName: "chromium",
+    baseURL: "localhost:3000",
+    headless: true
+  },
+  projects: [{
+    name: "chromium",
+    use: { browserName: "chromium" }
+  }]
+})
